1acf78fd13671c4559f95f940e4a95cd3ed819e6
Edward Cheng <edward@cheng.sydney>
2024-06-03 23:25:00 +1000

    Add installation script for Homer

--- scripts/installation/homer.sh
+++ scripts/installation/homer.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
-## Homepage: https://charts.gabe565.com/charts/homer/
-## Source Code: https://github.com/bastienwirtz/homer
-
-helm install homer gabe565/homer -f values.yaml
+helm upgrade --install ../charts/homer -n homer --create-namespace --values ../charts/homer/values-home-pi-cluster.yaml
