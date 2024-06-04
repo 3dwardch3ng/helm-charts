9521f509fa93c9ee96be2470f9f2820d09d80637
Edward Cheng <edward@cheng.sydney>
2024-06-04 16:57:54 +1000

    chore(pipeline): fix pipeline generate-gh-pages.yml

--- scripts/update-helm-repo.sh
+++ scripts/update-helm-repo.sh
@@ -9,8 +9,11 @@ helm repo add argo https://argoproj.github.io/argo-helm
 ## Grafana Helm Chart Repo
 helm repo add grafana https://grafana.github.io/helm-charts
 
+## Prometheus Helm Chart Repo
+helm repo add prometheus https://prometheus-community.github.io/helm-charts
+
 ## gabe565 Helm Chart Repo
 ## includes: AdGuard Home, Homer
-helm repo add gabe565 https://charts.gabe565.com
+#helm repo add gabe565 https://charts.gabe565.com
 
 helm repo update
